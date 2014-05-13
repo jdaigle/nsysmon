a2b58a39c80ae0f769ed18efa2e0551ebcefd7a4
Joseph Daigle <joseph@cridion.com>
2014-05-13 12:35:12 -0400

    loading node config from a JSON file

--- NSysmon.Core/Program.cs
+++ NSysmon.Core/Program.cs
@@ -1,39 +1,60 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
 using NSysmon.Core.Api;
-using NSysmon.Core.WMI;
 
 namespace NSysmon.Core
 {
     class Program
     {
+        public class JSONConfig
+        {
+            public JSONConfigNode[] nodes { get; set; }
+        }
+
+        public class JSONConfigNode
+        {
+            public string type { get; set; }
+            public string name { get; set; }
+            public object settings { get; set; }
+        }
+
         static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
-            PollingEngine.StartPolling();
-            PollingEngine.TryAdd(new WindowsServerNode("127.0.0.1", new WindowsServerNodeSettings()
+
+            var nodeConfigString = File.ReadAllText("config.json");
+            var settings = new Newtonsoft.Json.JsonSerializerSettings
             {
-                Host = "127.0.0.1"
-            }));
-            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+                TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto,
+            };
+            var nodeConfig = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONConfig>(nodeConfigString, settings);
+            foreach (var node in (nodeConfig.nodes ?? new JSONConfigNode[0]))
+            {
+                var pollNode = Activator.CreateInstance(typeof(PollingEngine).Assembly.GetType(node.type), new object[] { node.name, node.settings }) as PollNode;
+                PollingEngine.TryAdd(pollNode);
+            }
+            PollingEngine.StartPolling();
+
+            var hostConfig = new HttpSelfHostConfiguration("http://localhost:8080");
             // Remove the XML formatter
-            config.Formatters.Remove(config.Formatters.XmlFormatter);
-            config.Formatters.Add(new RazorFormatter());
-            config.Formatters.JsonFormatter.SerializerSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            config.Formatters.JsonFormatter.SerializerSettings.Error += (x, y) =>
+            hostConfig.Formatters.Remove(hostConfig.Formatters.XmlFormatter);
+            hostConfig.Formatters.Add(new RazorFormatter());
+            hostConfig.Formatters.JsonFormatter.SerializerSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+            hostConfig.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            hostConfig.Formatters.JsonFormatter.SerializerSettings.Error += (x, y) =>
             {
                 return;
             };
-            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+            hostConfig.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
             // Attribute routing.
-            config.MapHttpAttributeRoutes();
-            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            hostConfig.MapHttpAttributeRoutes();
+            hostConfig.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
 
-            using (var server = new HttpSelfHostServer(config))
+            using (var server = new HttpSelfHostServer(hostConfig))
             {
                 server.OpenAsync().Wait();
                 Console.WriteLine("Press Enter to quit.");
