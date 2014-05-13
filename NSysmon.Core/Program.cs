3847792aa798064c8d8c4d4a7007d5211e8d7e51
Joseph Daigle <joseph@cridion.com>
2014-05-13 12:50:31 -0400

    changing the process to be a service using Topshelp

--- NSysmon.Core/Program.cs
+++ NSysmon.Core/Program.cs
@@ -5,61 +5,29 @@ using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
 using NSysmon.Core.Api;
+using Topshelf;
 
 namespace NSysmon.Core
 {
-    class Program
+    public class Program
     {
-        public class JSONConfig
-        {
-            public JSONConfigNode[] nodes { get; set; }
-        }
-
-        public class JSONConfigNode
-        {
-            public string type { get; set; }
-            public string name { get; set; }
-            public object settings { get; set; }
-        }
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
-
-            var nodeConfigString = File.ReadAllText("config.json");
-            var settings = new Newtonsoft.Json.JsonSerializerSettings
+            var returnCode = HostFactory.Run(x =>
             {
-                TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto,
-            };
-            var nodeConfig = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONConfig>(nodeConfigString, settings);
-            foreach (var node in (nodeConfig.nodes ?? new JSONConfigNode[0]))
-            {
-                var pollNode = Activator.CreateInstance(typeof(PollingEngine).Assembly.GetType(node.type), new object[] { node.name, node.settings }) as PollNode;
-                PollingEngine.TryAdd(pollNode);
-            }
-            PollingEngine.StartPolling();
-
-            var hostConfig = new HttpSelfHostConfiguration("http://localhost:8080");
-            // Remove the XML formatter
-            hostConfig.Formatters.Remove(hostConfig.Formatters.XmlFormatter);
-            hostConfig.Formatters.Add(new RazorFormatter());
-            hostConfig.Formatters.JsonFormatter.SerializerSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
-            hostConfig.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            hostConfig.Formatters.JsonFormatter.SerializerSettings.Error += (x, y) =>
-            {
-                return;
-            };
-            hostConfig.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
-            // Attribute routing.
-            hostConfig.MapHttpAttributeRoutes();
-            hostConfig.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
-
-            using (var server = new HttpSelfHostServer(hostConfig))
-            {
-                server.OpenAsync().Wait();
-                Console.WriteLine("Press Enter to quit.");
-                Console.ReadLine();
-            }
+                x.Service<ServiceHost>(s =>
+                {
+                    s.ConstructUsing(name => new ServiceHost());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsPrompt();
+                x.StartAutomaticallyDelayed();
+                x.SetDescription("NSysmon Daemon");
+                x.SetDisplayName("NSysmon Daemon");
+                x.SetServiceName("NSysmon");
+            });
         }
     }
 }
---
+++ NSysmon.Core/ServiceHost.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using NSysmon.Core.Api;
+namespace NSysmon.Core
+{
+    public class ServiceHost
+    {
+        public class JSONConfig
+        {
+            public JSONConfigNode[] nodes { get; set; }
+        }
+
+        public class JSONConfigNode
+        {
+            public string type { get; set; }
+            public string name { get; set; }
+            public object settings { get; set; }
+        }
+
+        private EventWaitHandle stopServerSignal;
+
+        public void Stop()
+        {
+            stopServerSignal.Set();
+        }
+
+        public void Start()
+        {
+            var nodeConfigString = File.ReadAllText("config.json");
+            var settings = new Newtonsoft.Json.JsonSerializerSettings
+            {
+                TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto,
+            };
+            var nodeConfig = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONConfig>(nodeConfigString, settings);
+            foreach (var node in (nodeConfig.nodes ?? new JSONConfigNode[0]))
+            {
+                var pollNode = Activator.CreateInstance(typeof(PollingEngine).Assembly.GetType(node.type), new object[] { node.name, node.settings }) as PollNode;
+                PollingEngine.TryAdd(pollNode);
+            }
+            PollingEngine.StartPolling();
+
+            var hostConfig = new HttpSelfHostConfiguration("http://localhost:8080");
+            // Remove the XML formatter
+            hostConfig.Formatters.Remove(hostConfig.Formatters.XmlFormatter);
+            hostConfig.Formatters.Add(new RazorFormatter());
+            hostConfig.Formatters.JsonFormatter.SerializerSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+            hostConfig.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            hostConfig.Formatters.JsonFormatter.SerializerSettings.Error += (x, y) =>
+            {
+                return;
+            };
+            hostConfig.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+            // Attribute routing.
+            hostConfig.MapHttpAttributeRoutes();
+            hostConfig.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
+            using (var server = new HttpSelfHostServer(hostConfig))
+            {
+                server.OpenAsync().Wait();
+                stopServerSignal = new ManualResetEvent(false);
+                stopServerSignal.WaitOne();
+                // stop server
+                server.CloseAsync().Wait();
+            }
+            // stop polling
+            PollingEngine.StopPolling();
+        }
+    }
+}
