d2ff2d7681671a753206849f51b88424976b020c
Joseph Daigle <joseph@cridion.com>
2014-05-14 18:39:35 -0400

    tweaking config

--- NSysmon.Core/ServiceHost.cs
+++ NSysmon.Core/ServiceHost.cs
@@ -16,12 +16,13 @@ namespace NSysmon.Core
             public JSONConfig()
             {
                 nodes = new JSONConfigNode[0];
-                syslogListenerEnabled = true;
+                syslog = new JSONConfigSyslog();
+                http = new JSONConfigHttp();
             }
 
             public JSONConfigNode[] nodes { get; set; }
-            public bool syslogListenerEnabled { get; set; }
-            public int syslogListenerPort { get; set; }
+            public JSONConfigSyslog syslog { get; set; }
+            public JSONConfigHttp http { get; set; }
         }
 
         public class JSONConfigNode
@@ -29,6 +30,27 @@ namespace NSysmon.Core
             public string type { get; set; }
             public string name { get; set; }
             public object settings { get; set; }
+        }
+
+        public class JSONConfigSyslog
+        {
+            public JSONConfigSyslog()
+            {
+                listenerEnabled = true;
+            }
+
+            public bool listenerEnabled { get; set; }
+            public int listenerPort { get; set; }
+        }
+
+        public class JSONConfigHttp
+        {
+            public JSONConfigHttp()
+            {
+                listenerPort = 8080;
+            }
+
+            public int listenerPort { get; set; }
         }
 
         private EventWaitHandle stopServerSignal;
@@ -45,22 +67,22 @@ namespace NSysmon.Core
             {
                 TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto,
             };
-            var nodeConfig = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONConfig>(nodeConfigString, settings);
-            foreach (var node in (nodeConfig.nodes ?? new JSONConfigNode[0]))
+            var config = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONConfig>(nodeConfigString, settings);
+            foreach (var node in (config.nodes ?? new JSONConfigNode[0]))
             {
                 var pollNode = Activator.CreateInstance(typeof(PollingEngine).Assembly.GetType(node.type), new object[] { node.name, node.settings }) as PollNode;
                 PollingEngine.TryAdd(pollNode);
             }
             PollingEngine.StartPolling();
 
-            if (nodeConfig.syslogListenerEnabled)
+            if (config.syslog.listenerEnabled)
             {
                 var listener = new SyslogListener();
                 Action<int> startListener = listener.Start;
                 startListener.BeginInvoke(514, null, null);
             }
 
-            var hostConfig = new HttpSelfHostConfiguration("http://localhost:8080");
+            var hostConfig = new HttpSelfHostConfiguration("http://127.0.0.1:"+config.http.listenerPort);
             // Remove the XML formatter
             hostConfig.Formatters.Remove(hostConfig.Formatters.XmlFormatter);
             hostConfig.Formatters.Add(new RazorFormatter());
