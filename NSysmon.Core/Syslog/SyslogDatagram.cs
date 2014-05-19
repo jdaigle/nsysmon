685ad3155af3dd9b8a5e3ec5ab7663067fc99d0e
Joseph Daigle <joseph@cridion.com>
2014-05-19 18:20:35 -0400

    more syslog stuff

---
+++ NSysmon.Core/Syslog/SyslogDatagram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSysmon.Core.Syslog
+{
+    public class SyslogDatagram
+    {
+        public SyslogDatagram(string datagram, int severity, int facility, DateTime sentDateTime, string sourceIPAddress)
+        {
+            this.Datagram = datagram;
+            this.Severity = severity;
+            this.Facility = facility;
+            this.SentDateTime = sentDateTime;
+            this.SourceIPAddress = sourceIPAddress;
+        }
+
+        public string Datagram { get; private set; }
+        public int Severity { get; private set; }
+        public int Facility { get; private set; }
+        public DateTime SentDateTime { get; private set; }
+        public string SourceIPAddress { get; private set; }
+    }
+}
